0910352b698a9367075e04360fbefe74228efbbf
Soyeb189 <muktadir189@gmai.com>
2020-08-20 13:49:08 +0600

    Side menu

--- app/src/main/java/com/bankasia/smartapp/Home.kt
+++ app/src/main/java/com/bankasia/smartapp/Home.kt
@@ -1,11 +1,49 @@
 package com.bankasia.smartapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class Home : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var imageViewHeaderMenuClose: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.draver_layout)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        val hView = navigationView.getHeaderView(0)
+        imageViewHeaderMenuClose = hView.findViewById(R.id.closeHeader)
+        imageViewHeaderMenuClose.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuToUse = R.menu.my_right_side_menu
+        val inflater = menuInflater
+        inflater.inflate(menuToUse, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun Menu(item: MenuItem?) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.END)
+        }
     }
 }
